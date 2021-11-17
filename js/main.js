7e9f6270d365d92edae268c73aa6786d46e57c72
devtovi <cameron@tovigroup.com>
2021-11-17 20:13:34 +1000

    styling

--- js/main.js
+++ js/main.js
@@ -212,19 +212,7 @@
 	};
 
 	 
-  /* Leaflet Map
-	* ------------------------------------------------------ */
-    var mymap = L.map('map').setView([51.505, -0.09], 13);
 
-    	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    	maxZoom: 18,
-    	id: 'mapbox/streets-v11',
-    	tileSize: 512,
-    	zoomOffset: -1,
-    	accessToken: 'pk.eyJ1IjoidG92aWZvb2RzIiwiYSI6ImNramdxYzZ2cDRkM3AyeXBrYzhqMjNlbXAifQ.FPN9mRgVcnaedcFdL6Cg6g'
-    
-	}).addTo(mymap);
 
 
  
